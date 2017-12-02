1ca92e5f81f38cdb2164805d64b4a5b54c01647a
admin <admin@OSMARVM0>
2017-12-02 12:57:20 -0200

    Correção de Comentários

--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,7 @@
  * Universidade Federal de Santa Catarina - UFSC
  * Departamento de Informática e Estatística - INE
  * Programa de Pós-Graduação em Ciências da Computação - PROPG
- * Disciplinas: Projeto e Análise de Algoritmos
+ * Disciplina: Projeto e Análise de Algoritmos
  * Prof Alexandre Gonçalves da Silva 
  *
  * Baseado nos slides 36 da aula do dia 18/08/2017 
